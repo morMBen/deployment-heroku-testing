ead7237b580f417b04a9e5b4989260ff014f1252
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2022-01-20 23:30:47 +0200

    testing

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 // console.log(__dirname.split('\\').slice(0, -1).join('\\'));
 // const publicPath = path.join(__dirname, '../', 'client/build');
 const publicPath = path.join(
-  __dirname.split('/').slice(0, -1).join('/'),
+  __dirname.split('\\').slice(0, -1).join('\\'),
   'client/build'
 );
 app.use(cors());
