1733eaef8ddfedb7ecb6124d397ec595be72bc2c
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2022-01-20 23:08:59 +0200

    testin

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //
-const publicPath = path.join(__dirname, 'client/build');
+const publicPath = path.join(__dirname, '../client/build');
 app.use(cors());
 app.use(express.static(publicPath));
 //
