8a68ef00fa054ed65638e5cc2d2dc0c33795eaa0
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2022-01-23 10:09:13 +0200

    rest

--- server/app.js
+++ server/app.js
@@ -25,9 +25,9 @@ app.get('/api/users', (req, res) => {
 });
 
 //
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+// });
 //
 
 app.listen(port, () => {
